d67cc71d233602df495e89aa3be7e07484d8da0d
Aliansyah Firdaus <aliansyah.it@gmail.com>
2022-04-19 11:44:51 +0700

    Change file router/index and index.html

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+import HomePage from "../views/homePage.vue";
+import LoginPage from "../views/loginPage.vue";
+import RegisterPage from "../views/registerPage.vue";
+import ProfilePage from "../views/profilePage.vue";
+import Detailpage from "../views/detailTourPage.vue";
+import TagPage from "../views/tagPage.vue";
+import CountryPage from "../views/countryPage.vue";
+
+const routes = [
+  { path: "/", name: "home-page", component: HomePage },
+  { path: "/login", name: "login-page", component: LoginPage },
+  { path: "/register", name: "register-page", component: RegisterPage },
+  { path: "/profile", name: "profile-page", component: ProfilePage },
+  { path: "/detail/:id", name: "detail-page", component: Detailpage },
+  { path: "/tag/:name", name: "tag-page", component: TagPage },
+  { path: "/country/:name", name: "contry-page", component: CountryPage },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
